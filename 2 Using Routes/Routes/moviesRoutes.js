70b526c56ccf6b0c4d1078884504c6eadfdadee9
Anirudh Yakala <103211883+yakalaanirudh@users.noreply.github.com>
2024-06-27 20:32:47 +0530

    Add files via upload

---
+++ 2 Using Routes/Routes/moviesRoutes.js
@@ -0,0 +1,27 @@
+//This file is going to contain all the routes for movies
+
+const express=require('express')
+
+const moviesController=require('./../Controller/moviesController')
+
+
+
+//const moviesRouter= express.Router()  //Instead of calling moviesRouter just call it router
+const router= express.Router()
+
+
+
+
+//moviesRouter.route('/') //This /(url) is appended to the url in the moviesRouter url
+router.route('/')
+.get(moviesController.getAllMovies)
+.post(moviesController.createMovie)
+
+//moviesRouter.route('/:id')  //This /:id(url) is appended to the url in the moviesRouter url
+router.route('/:id')
+.get(moviesController.getSingleMovie)
+.patch(moviesController.updateMovie)
+.delete(moviesController.deleteMovie)
+
+
+module.exports=router
